606bd410d9af4e440e24d4d4d896e99737871fcc
xiaosongHQ <1520339193@qq.com>
2018-09-04 22:56:10 +0800

    创建控制器，创建部分路由规则

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/index','AdminController@index');//后台首页
+
+
+//后台的后台
+Route::resource('user', 'UserController');//用户
+Route::resource('a_d', 'ADController');//广告
+Route::resource('webset', 'WebSetController');//网站设置
+Route::resource('link', 'LinkController');//友情链接
+Route::resource('proposal', 'ProposalController');//建议
+Route::resource('help', 'HelpController');//帮助
+Route::resource('gift', 'GiftController');//礼物
+
+//前台的后台
+Route::resource('comment', 'CommentController');//留言
+Route::resource('gift_gx', 'GiftGxController');//礼物关系
+Route::resource('option', 'OptionController');//选项
+Route::resource('vote', 'VoteController');//投票
+
+
