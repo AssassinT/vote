aa601d82b898f0a9180c77367e907a89b1e837a0
lyl <lyl@qq.com>
2018-09-05 17:02:06 +0800

    友链增删改查

---
+++ resources/views/admin/link/index.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.admin')  
+@section('content')
+<div class="tpl-portlet-components">
+    <div class="portlet-title">
+        <div class="caption font-green bold">
+            <span class="am-icon-code"></span>友情链接列表
+        </div>
+    </div>
+    <div class="tpl-block">
+        <div class="am-g">
+            <div class="am-u-sm-12 am-u-md-6">
+                <div class="am-btn-toolbar">
+                    <div class="am-btn-group am-btn-group-xs">
+                        <a href="/link/create" class="am-btn am-btn-default am-btn-success"><span class="am-icon-plus"></span> 新增</a>
+                        <button type="button" class="am-btn am-btn-default am-btn-danger"><span class="am-icon-trash-o"></span> 删除</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="am-u-sm-12 am-u-md-3">
+                <form action="/link" method="get">
+                <div class="am-input-group am-input-group-sm">
+                    <input type="text" name="keywords" class="am-form-field" value="{{request()->keywords}}">
+                    <span class="am-input-group-btn">
+                        <button class="am-btn  am-btn-default am-btn-success tpl-am-btn-success am-icon-search"></button>
+                      </span>
+                </div>
+                </form>
+            </div>
+        </div>
+        <div class="am-g">
+            <div class="am-u-sm-12">
+                <table class="am-table am-table-striped am-table-hover table-main">
+                    <thead>
+                        <tr>
+                            <th class="table-check">
+                                <input type="checkbox" class="tpl-table-fz-check">
+                            </th>
+                            <th class="table-id">ID</th>
+                            <th class="table-title">名称</th>
+                            <th class="table-title">权重</th>
+                            <th class="table-title">链接LOGO</th>
+                            <th class="table-title">链接地址</th>
+                            <th class="table-set">操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($links as $v)
+                        <tr>
+                            <td>
+                                <input type="checkbox">
+                            </td>
+                            <td>{{$v['id']}}</td>
+                            <td class="am-hide-sm-only">{{$v['link_name']}}</td>
+                            <td class="am-hide-sm-only">{{$v['weight']}}</td>
+                            <td class="am-hide-sm-only"><img src="{{$v['link_pic']}}" width="50" alt="" height="50"></td>
+                            <td class="am-hide-sm-only">{{$v['link_url']}}</td>
+                            <td>
+                                <div class="am-btn-toolbar">
+                                    <div class="am-btn-group am-btn-group-xs">
+                                        <a href="/link/{{$v['id']}}/edit" class="am-btn am-btn-default am-btn-xs am-text-secondary"><span class="am-icon-pencil-square-o"></span> 编辑</a>
+                                        <form style="float:left" action="/link/{{$v['id']}}" method="post">
+                                            {{method_field('DELETE')}}
+                                            {{csrf_field()}}
+                                            <button class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span class="am-icon-trash-o"></span> 删除</button>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                
+            </div>
+        </div>
+    </div>
+    <div class="tpl-alert"></div>
+</div>
+@endsection
