034f5653555d62279d949dae7082ec54cb964d02
xiaosongHQ <1520339193@qq.com>
2018-09-07 09:22:52 +0800

    删除多余的code

--- code/app/2-substr.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$str = '我0爱1你亲爱的菇凉!!';
-
-	// $res = substr($str, 0, 6);
-	$res = mb_substr($str, 0, 6, 'utf-8');
-
-	echo $res;
--- code/common.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$redis = new Redis;
-
-	$res = $redis -> connect('localhost',6379);
-
-	$redis -> auth('123123');
-
-	$redis -> select(1);
