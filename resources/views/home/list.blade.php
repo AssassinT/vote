a0c5875bf03092621e1a815c229e99b8ce22af5c
xiaosongHQ <1520339193@qq.com>
2018-09-10 17:58:45 +0800

    增加了类型字段，list列表显示

--- resources/views/home/list.blade.php
+++ resources/views/home/list.blade.php
@@ -12,12 +12,12 @@
 					<th>总票数</th>
 					<th>操作</th>
 				</tr>
-
+@foreach($votes as $v)
 				<tr class="active">
-					<td>1</td>
-					<td>lamp207班花选举投票活动</td>
-					<td>投票中</td>
-					<td>2018-09-02</td>
+					<td>{{$v->id}}</td>
+					<td>{{$v->vote_title}}</td>
+					<td>待定义</td>
+					<td>{{$v->created_at}}</td>
 					<td>12</td>
 					<td>520</td>
 					<td>
@@ -27,47 +27,7 @@
 					</td>
 				</tr>
 
-				<tr class="active">
-					<td>1</td>
-					<td>lamp207班花选举投票活动</td>
-					<td>投票中</td>
-					<td>2018-09-02</td>
-					<td>12</td>
-					<td>520</td>
-					<td>
-						<a href="/vote/1/edit">修改</a>|
-						<a href="#">统计</a>|
-						<a href="#">删除</a>
-					</td>
-				</tr>
-
-				<tr class="active">
-					<td>1</td>
-					<td>lamp207班花选举投票活动</td>
-					<td>投票中</td>
-					<td>2018-09-02</td>
-					<td>12</td>
-					<td>520</td>
-					<td>
-						<a href="/vote/1/edit">修改</a>|
-						<a href="#">统计</a>|
-						<a href="#">删除</a>
-					</td>
-				</tr>
-
-				<tr class="active">
-					<td>1</td>
-					<td>lamp207班花选举投票活动</td>
-					<td>投票中</td>
-					<td>2018-09-02</td>
-					<td>12</td>
-					<td>520</td>
-					<td>
-						<a href="/vote/1/edit">修改</a>|
-						<a href="#">统计</a>|
-						<a href="#">删除</a>
-					</td>
-				</tr>
+@endforeach
 				
 			</table>	
 @endsection
