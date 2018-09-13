ac003dd31dde8ec5a483982e87c033cd1714f378
huangrj <949815699@qq.com>
2018-09-13 16:34:29 +0800

    退出

--- resources/views/layouts/home/login.blade.php
+++ resources/views/layouts/home/login.blade.php
@@ -1,11 +1,37 @@
+			<style>
+				.active{
+					display:block;
+				}
+				#sell{
+					width:80px;
+					height:80px;
+					position:absolute;
+					top:50px;
+					display:none;
+				}
+			</style>	
 			@if(!Session::has('id'))
 			<div class='login'>
-				<div class='head_pic'></div>
 				<span>&nbsp; &nbsp; <a href="/home/login">登录111</a></span>
 			</div>
 			@else
 			<div class='login'>
-			<div class='head_pic'></div>
-			<a href="/myindex"><button type="button"  class="btn btn-warning">个人中心</button></a>
+			<div class='head_pic' id="sel">
+				<div id="sell">
+				<a href="/myindex">个人中心</a><br>
+				<a href="/loginout">退出登录</a>
+				</div>
+			</div>
+			
 			</div>
 			@endif
+
+			<script>
+			$('#sel').click(function(){
+				if($('#sell').css('display')=='none'){
+				$('#sell').show();
+			}else{
+				$('#sell').hide();
+			}
+			})
+			</script>
