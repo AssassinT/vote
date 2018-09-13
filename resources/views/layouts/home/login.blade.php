2b7eff639ad1f215e0d3c6583f4dfd9056cef733
huangrj <949815699@qq.com>
2018-09-13 16:32:46 +0800

    登录

--- resources/views/layouts/home/login.blade.php
+++ resources/views/layouts/home/login.blade.php
@@ -1,7 +1,11 @@
-
+			@if(!Session::has('id'))
 			<div class='login'>
 				<div class='head_pic'></div>
-				<span>&nbsp; &nbsp; <a href="/home/login">登录</a></span>
+				<span>&nbsp; &nbsp; <a href="/home/login">登录111</a></span>
 			</div>
-
-			
+			@else
+			<div class='login'>
+			<div class='head_pic'></div>
+			<a href="/myindex"><button type="button"  class="btn btn-warning">个人中心</button></a>
+			</div>
+			@endif
