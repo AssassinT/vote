10a116db682ad0447eb4e3d680f697d4e8d47958
xiaosongHQ <1520339193@qq.com>
2018-09-14 11:44:18 +0800

    wechat

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use EasyWeChat\Factory;
+class WechatController extends Controller
+{
+    public function index(){
+
+		$config = [
+		    'app_id' => 'wxeb8503aed05a2c1a',
+		    'secret' => '052836c4dde956a0644acf0607c8934d',
+		    'token' => 'wechat',
+		    'response_type' => 'array',
+
+		    'log' => [
+		        'level' => 'debug',
+		        'file' => __DIR__.'/wechat.log',
+		    ],
+		];
+
+		$app = Factory::officialAccount($config);
+
+		$response = $app->server->serve();
+
+		// 将响应输出
+		return $response;
+    }
+}
